f1282b9041c143904d66555758abf9518b881fa2
ZisanSarker <sakibuzzamanjisan@gmail.com>
2024-10-30 19:21:03 +0600

    shadcn cli is added

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,11 +53,7 @@ export const metadata: Metadata = {
   description: "Pitch, Vote and Grow",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
       <body
