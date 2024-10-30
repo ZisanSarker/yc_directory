6bffacfffc801f809f1c8950351d61f652cf30d7
ZisanSarker <sakibuzzamanjisan@gmail.com>
2024-10-30 22:08:48 +0600

    seach section added in hero

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,23 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
-    <section className="pink_container">
-      <h1 className="heading">Pitch Your Startup, <br/> Connect With Enterpreneurs</h1>
-      <p className="sub-heading !max-w-3xl">
-        Submit Ideas, Vote on Picthes, and Get Noticed in virtual Competitions.
-      </p>
-    </section>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Enterpreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Picthes, and Get Noticed in virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
